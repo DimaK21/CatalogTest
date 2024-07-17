54c29dd8d3e49bf4c172f6bca85ff07ef81484d5
Dmitry Kryushnikov <civ02@mail.ru>
2024-07-18 00:53:27 +0500

    файлы для экранов, viewmodel

---
+++ app/src/main/java/ru/kryu/catalogtest/screen/productlist/ProductListScreen.kt
@@ -0,0 +1,2 @@
+package ru.kryu.catalogtest.screen.productlist
+
---
+++ app/src/main/java/ru/kryu/catalogtest/screen/subcategories/SubcategoriesViewModel.kt
@@ -0,0 +1,9 @@
+package ru.kryu.catalogtest.screen.subcategories
+
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class SubcategoriesViewModel @Inject constructor() : ViewModel() {
+}
